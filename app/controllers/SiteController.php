e1864937ed7c976fedad3178a70227f53b9424b9
idearr <295136728@qq.com>
2016-07-11 17:40:36 +0800

    app test

--- app/controllers/SiteController.php
+++ app/controllers/SiteController.php
@@ -22,7 +22,7 @@ class SiteController extends Controller
                 'class' => AccessControl::className(),
                 'rules' => [
                     [
-                        'actions' => ['login', 'error', 'index'],
+                        'actions' => ['login', 'error', 'index', 'test'],
                         'allow' => true,
                     ],
 					/**
